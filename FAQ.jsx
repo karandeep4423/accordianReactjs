69b4476050bc734544f8a44fb5d9d50bd3529f45
vivek-fulldev <vivekprasad2992@gmail.com>
2021-09-03 16:59:14 +0530

    accordian project has been added

---
+++ FAQ.jsx
@@ -0,0 +1,21 @@
+import React ,{useState} from 'react'
+import './style.css';
+import rightIcon from "./right-arrow.png"
+function FAQ({question}) {
+    const [state, setstate] = useState(true)
+    function changeView() {
+         console.log("change view function have been clicked");
+         //setstate(prev=> prev= !prev);
+         setstate(!state)
+    }
+    console.count(question)
+    return (
+        <div className="faq-question">
+               <div className="question-body">
+                 <h3>{question.question}</h3>
+                 <img src={rightIcon} alt="icon" onClick={changeView} className={state ? "transformIcon" : ""} />
+               </div>
+                <p className={state ? "hidePtag" : "showPtag"}>{question.answer}</p>
+        </div>
+    )}   
+export default FAQ
---
+++ QuestionAndAnswer.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import FAQ from './FAQ'
+import './style.css';
+function QuestionAndAnswer() {
+  
+    let faQArrays = [
+        {
+          question: "Tell me about yourself?",
+          answer : "ipsam ipsum animi rerum quisquam expedita consectetur pariatur asperiores quasi! Soluta, ex eligendi. Reprehenderit id deserunt in accusantium cum vero tenetur laudantium animi dolorem ad quis possimus quo odio laborum consequatur rem eius, eaque fugiat est, eligendi iste dicta et? Repellat dolorem perferendis facilis, ab dolores quo ut distinctio."            
+        },
+        {
+            question: "Tell me about yourself?",
+            answer : "ipsam ipsum animi rerum quisquam expedita consectetur pariatur asperiores quasi! Soluta, ex eligendi. Reprehenderit id deserunt in accusantium cum vero tenetur laudantium animi dolorem ad quis possimus quo odio laborum consequatur rem eius, eaque fugiat est, eligendi iste dicta et? Repellat dolorem perferendis facilis, ab dolores quo ut distinctio."            
+          },
+        {
+            question: "Tell me about yourself?",
+            answer : "ipsam ipsum animi rerum quisquam expedita consectetur pariatur asperiores quasi! Soluta, ex eligendi. Reprehenderit id deserunt in accusantium cum vero tenetur laudantium animi dolorem ad quis possimus quo odio laborum consequatur rem eius, eaque fugiat est, eligendi iste dicta et? Repellat dolorem perferendis facilis, ab dolores quo ut distinctio."            
+          },
+    ]
+
+    return (
+        <section className="faq-body">
+             <h2 className="faq-heading">Frequently Asked Question</h2>
+             {/* {faQArrays.map((question,i)=> <FAQ question={question} key={i}/>)} */}
+            <FAQ question={faQArrays[0]} />
+            <FAQ question={faQArrays[1]} />
+            <FAQ question={faQArrays[2]} />
+
+        </section>
+    )
+}
+
+export default QuestionAndAnswer
